b78824889e029ee433027a1439d0ea046384f456
ksgkodz <gamer_x420x@proton.me>
2025-06-01 12:48:49 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Home } from './Pages/Home'
+import { NotFound } from './Pages/NotFound'
+
+const App = () => {
+  return (
+    <>
+    <BrowserRouter>
+      <Routes>
+          <Route index element= {<Home/>} />
+          <Route path= "*" element= {<NotFound/>} />
+      </Routes>
+    </BrowserRouter>
+    </>
+  )
+}
+
+export default App
